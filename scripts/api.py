7c97b4efc723aef0f514e576698e13c9ca89ea1d
Hetaneko <54069666+Hetaneko@users.noreply.github.com>
2024-10-15 22:02:53 +0300

    Update api.py

--- scripts/api.py
+++ scripts/api.py
@@ -91,7 +91,6 @@ def dtg_api(_: gr.Blocks, app: FastAPI):
         # file = models.download_gguf(gguf_name="ggml-model-Q6_K.gguf")
         files = models.list_gguf()
         file = files[-1]
-        logger.info(f"Use gguf model from local file: {file}")
         models.load_model(file, gguf=True, device="cpu")
         # models.load_model()
         # models.text_model.half().cuda()
